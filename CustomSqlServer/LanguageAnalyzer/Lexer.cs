1fa8093f426faae70c6bce3074bb00ef114f9d09
gvelesa <giorgiko@list.ru>
2023-10-07 18:01:41 +0400

    Add project files.

---
+++ CustomSqlServer/LanguageAnalyzer/Lexer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CustomSqlServer.LanguageAnalyzer
+{
+    internal class SqlLexer
+    {
+        private readonly string _input;
+        private int _position;
+        private static readonly Regex IdentifierRegex = new Regex();
+    }
+}
---
+++ CustomSqlServer/LanguageAnalyzer/SqlToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomSqlServer.LanguageAnalyzer
+{
+    public class SqlToken
+    {
+        public SqlToken(SqlTokenKind kind, string text)
+        {
+            Kind = kind;
+            Text = text;
+        }
+
+        public SqlTokenKind Kind { get; }
+        public string Text { get; }
+    }
+
+}
